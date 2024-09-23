4ce0df2f2b2e733e877fe7cb7193cc065de32410
adel boullif <adoula136@gmail.com>
2024-09-23 23:50:08 +0200

    fdfs

--- src/app/payment/page.jsx
+++ src/app/payment/page.jsx
@@ -2,7 +2,8 @@
 import React from "react"
 import { loadStripe } from "@stripe/stripe-js"
 import { Elements } from "@stripe/react-stripe-js"
-import CheckoutForm from "../components/CheckoutForm"
+// import CheckoutForm from "../components/CheckoutForm"
+import CheckoutForm from "../components/CheckoutForm.jsx"
 
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY)
 
