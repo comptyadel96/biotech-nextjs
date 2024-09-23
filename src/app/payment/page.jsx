727e2e19066d359a2c005c67445ad9b359aabfc7
adel boullif <adoula136@gmail.com>
2024-09-23 22:40:12 +0200

    fixing import errror

--- src/app/payment/page.jsx
+++ src/app/payment/page.jsx
@@ -1,73 +1,4 @@
 "use client"
-// import React from "react"
-// import { loadStripe } from "@stripe/stripe-js"
-// import { Elements } from "@stripe/react-stripe-js"
-// import CheckoutForm from "../components/CheckoutForm"
-// import CompletePage from "../components/CompletePage"
-
-// const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY)
-
-// export default function App() {
-//   const [clientSecret, setClientSecret] = React.useState("")
-//   const [dpmCheckerLink, setDpmCheckerLink] = React.useState("")
-//   const [confirmed, setConfirmed] = React.useState(false)
-
-//   React.useEffect(() => {
-//     setConfirmed(
-//       new URLSearchParams(window.location.search).get(
-//         "payment_intent_client_secret"
-//       )
-//     )
-//   }, [])
-
-//   const items = [
-//     { id: 1, price: 1000, quantity: 2 },
-//     { id: 2, price: 500, quantity: 1 },
-//   ]
-//   React.useEffect(() => {
-//     // Create PaymentIntent as soon as the page loads
-//     fetch("/api/create-payment-intent", {
-//       method: "POST",
-//       headers: { "Content-Type": "application/json" },
-//       body: JSON.stringify({ items}),
-//     })
-//       .then((res) => res.json())
-//       .then((data) => {
-//         setClientSecret(data.clientSecret)
-//         // [DEV] For demo purposes only
-//         setDpmCheckerLink(data.dpmCheckerLink)
-//       })
-//   }, [])
-
-//   const appearance = {
-//     theme: "stripe",
-//     variables: {
-//       colorPrimary: "#0cd4bd",
-//       colorBackground: "#3d3d3d",
-//       colorText: "#ffffff",
-//     },
-//   }
-
-//   const options = {
-//     clientSecret,
-//     appearance,
-//   }
-
-//   return (
-//     <div className=" flex flex-col  lg:max-w-[70%] lg:py-10 mx-auto ">
-//       {clientSecret && (
-//         <Elements options={options} stripe={stripePromise}>
-//           {confirmed ? (
-//             <CompletePage />
-//           ) : (
-//             <CheckoutForm dpmCheckerLink={dpmCheckerLink} />
-//           )}
-//         </Elements>
-//       )}
-//     </div>
-//   )
-// }
-
 import React from "react"
 import { loadStripe } from "@stripe/stripe-js"
 import { Elements } from "@stripe/react-stripe-js"
@@ -169,7 +100,7 @@ export default function App() {
             <CheckoutForm dpmCheckerLink={dpmCheckerLink} />
           )}
         </Elements>
-      ) }
+      )}
     </div>
   )
 }
